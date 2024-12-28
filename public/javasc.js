d6699fb02fec6bfbf95a0c44f8279fa5d1e0ea63
Rohit Kumar Singh <153061748+rohsingh26@users.noreply.github.com>
2024-12-28 21:53:59 +0530

    Update javasc.js

--- public/javasc.js
+++ public/javasc.js
@@ -1,16 +1,13 @@
 function ope() {
-    // Get the input values
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
     const op = document.getElementById("op").value;
   
-    // Check if inputs are valid numbers
     if (isNaN(num1) || isNaN(num2)) {
       alert("Please enter valid numbers.");
       return;
     }
   
-    // Perform addition
     let r;
     switch (op) {
         case "+":
@@ -34,7 +31,6 @@ function ope() {
           return;
       }
   
-    // Display the result in the HTML
     document.getElementById("result").innerText = `Result: ${r}`;
   }
   
